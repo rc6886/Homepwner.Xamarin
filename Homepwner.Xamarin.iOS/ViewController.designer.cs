b0d770fa49e4bc859ae25d860f18b63f485fa608
rc6886 <rc6886@gmail.com>
2016-07-24 21:51:10 -0500

    Working on the AutoLayout on the detail screen. Everything looks aligned excep the "Value" label, which is off screen.

--- Homepwner.Xamarin.iOS/ViewController.designer.cs
+++ Homepwner.Xamarin.iOS/ViewController.designer.cs
@@ -1,17 +1,47 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace Homepwner.Xamarin.iOS
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Name { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Serial { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel Value { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (Name != null) {
+                Name.Dispose ();
+                Name = null;
+            }
+
+            if (Serial != null) {
+                Serial.Dispose ();
+                Serial = null;
+            }
+
+            if (Value != null) {
+                Value.Dispose ();
+                Value = null;
+            }
         }
     }
 }
