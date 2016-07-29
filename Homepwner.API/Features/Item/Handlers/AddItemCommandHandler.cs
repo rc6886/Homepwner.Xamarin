985e2004eadf363a78cda773895d151d42acf5a2
Rodney Coleman <rc6886@gmail.com>
2016-07-28 21:10:48 -0500

    Added skeleton command handler for adding items.

---
+++ Homepwner.API/Features/Item/Handlers/AddItemCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MediatR;
+
+namespace Homepwner.API
+{
+    public class AddItemCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public double Value { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class AddItemCommandHandler : RequestHandler<AddItemCommand>
+    {
+        public AddItemCommandHandler()
+        {
+        }
+
+        protected override void HandleCore(AddItemCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Homepwner.API/Features/Item/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Homepwner.API
+{
+    public class Item
+    {
+        public Item(Guid id, string name, string serialNumber, double value, DateTime dateCreated)
+        {
+            Id = id;
+            Name = name;
+            SerialNumber = serialNumber;
+            Value = value;
+            DateCreated = DateTime.Now;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string SerialNumber { get; set; }
+        public double Value { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
+
