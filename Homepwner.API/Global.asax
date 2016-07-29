99137d14930ac1bfd669d5f63cb1b512cf8cb34f
Rodney Coleman <rc6886@gmail.com>
2016-07-28 20:04:44 -0500

    Added initial working API with NancyFx.

---
+++ Homepwner.API/Global.asax
@@ -0,0 +1,2 @@
+﻿<%@ Application Inherits="Homepwner.API.Global" %>
+
---
+++ Homepwner.API/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+namespace Homepwner.API
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters => "Hello world";
+        }
+    }
+}
+
