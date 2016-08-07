d624d8b54e53c48482c53cb0e6134475d82efe69
rc6886 <rc6886@gmail.com>
2016-08-07 07:22:53 -0500

    Added self-hosting to the solution for the API.

---
+++ Homepwner.API.Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Homepwner.API.Host
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string url = "http://localhost:1986";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}...", url);
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
